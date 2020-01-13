6ddf9f738823854a5bec144ce9345cca10c75c28
Segun Alesinloye <segunalesh@gmail.com>
2020-01-12 19:28:06 -0600

    Got the background color to change with the user input

--- index.js
+++ index.js
@@ -53,6 +53,34 @@ inquirer
         data.forEach(element => console.log(element));
         // const repoList = res.data.map(repo => repo.name)
         // console.log(repoList.length)
+
+        const colors = {
+          green: {
+            wrapperBackground: "#E6E1C3",
+            headerBackground: "#C1C72C",
+            headerColor: "black",
+            photoBorderColor: "#black"
+          },
+          blue: {
+            wrapperBackground: "#5F64D3",
+            headerBackground: "#26175A",
+            headerColor: "white",
+            photoBorderColor: "#73448C"
+          },
+          pink: {
+            wrapperBackground: "#879CDF",
+            headerBackground: "#FF8374",
+            headerColor: "white",
+            photoBorderColor: "#FEE24C"
+          },
+          red: {
+            wrapperBackground: "#DE9967",
+            headerBackground: "#870603",
+            headerColor: "white",
+            photoBorderColor: "white"
+          }
+        };
+
         const html = `<!DOCTYPE html>
         <html lang="en">
         <head>
@@ -64,26 +92,27 @@ inquirer
         </head>
         <body>
         
-          <div class="container">
+          <div style="background-color: ${color}; color: ${color};" class="container">
             
-            <div class="jumbotron">
+            <div  style="background-color: white;" class="jumbotron">
               <div class="text-center">
                 <img style="border-radius: 300px;" src="https://picsum.photos/300/300" class="rounded" alt="...">
               </div>
               <h1 style="text-align: center;" class="display-4">Hello</h1>
               <p style="text-align: center;" class="lead">My name is ${name}</p>
               <hr class="my-4">
-              <p style="text-align: center; margin: 10px;"><a href="">Location</a> <a href="https://github.com/${username}">Github</a</p>
-              <div style="text-align: center;" class="row">
-                <a style="padding: 25px;" class="col-sm-5" class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+              <p style="color: ${color}; text-align: center; margin: 15px;"><a href="">Location</a> <a href="https://github.com/${username}">Github</a></p>
+              
+              <div style="text-align: center; color: white; " class="row">
+                <h5 style="padding: 15px; background-color: ${color};" class="col-sm-5">Learn more</h5>
                 <div class="col-sm-2"></div>
-                <a style="padding: 25px;" class="col-sm-5" class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <h5 style="padding: 15px; background-color: ${color};" class="col-sm-5">Learn more</h5>
               </div>
         
               <div style="text-align: center;" class="row">
-                <a style="padding: 25px;" class="col-sm-5" class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <h5 style="padding: 15px; background-color: ${color};" class="col-sm-5">Learn more</h5>
                 <div class="col-sm-2"></div>
-                <a style="padding: 25px;" class="col-sm-5" class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+                <h5 style="padding: 15px; background-color: ${color};" class="col-sm-5">Learn more</h5>
               </div>
             </div>   
           </div>
